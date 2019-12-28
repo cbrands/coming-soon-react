3c33aaab2c5092ca74304d7ed73357a1873e6f7b
Charles Brands <charles.brands@gmail.com>
2019-12-28 22:35:38 +0100

    styling social, copyright and plain text

--- src/components/Attribution.js
+++ src/components/Attribution.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import './Attribution.css';
 
 const Attribution = () =>  {
-  const greeting = 'Hello Attribution!';
-  return <h1>{greeting}</h1>;
+  return (
+    <footer>
+      <p className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer">Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/cbrands/coming-soon-react">Charles Brands</a>.
+      </p>
+    </footer>
+  )
 }
 
 export default Attribution;
--- src/components/Social.js
+++ src/components/Social.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './Social.css';
 
 
 const Social = () =>  {
-  const greeting = 'Hello Social!';
-  return <h1>{greeting}</h1>;
+  return (
+    <section className="social">
+      <a href="#"><i className="fab fa-facebook-f"></i><span className="sr-only">facebook</span></a>
+      <a href="#"><i className="fab fa-twitter"></i><span className="sr-only">twitter</span></a>
+      <a href="#"><i className="fab fa-instagram"></i><span className="sr-only">instagram</span></a>
+    </section>
+  )
 }
 
 export default Social;
