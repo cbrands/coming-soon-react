a3d627a36ca5d84cf62acc391f49604021646502
Charles Brands <charles.brands@gmail.com>
2019-12-29 19:38:44 +0100

    Updates

---
+++ precache-manifest.88ffa989f27f62d254fc78ce087b28fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be5b28add6334a4c74e850c217de669",
+    "url": "/coming-soon-react/index.html"
+  },
+  {
+    "revision": "f732b51a56a206a6be33",
+    "url": "/coming-soon-react/static/css/main.00b99afb.chunk.css"
+  },
+  {
+    "revision": "625b0c1d6c2d737afd95",
+    "url": "/coming-soon-react/static/js/2.635253fe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/coming-soon-react/static/js/2.635253fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f732b51a56a206a6be33",
+    "url": "/coming-soon-react/static/js/main.7f166380.chunk.js"
+  },
+  {
+    "revision": "f6e32630a67bbfd71eab",
+    "url": "/coming-soon-react/static/js/runtime-main.19869926.js"
+  },
+  {
+    "revision": "92dd5c8fe5e8dde945a6f01abca7e7cc",
+    "url": "/coming-soon-react/static/media/illustration-dashboard.92dd5c8f.png"
+  },
+  {
+    "revision": "ddf22f0b5d6907a8b879a12905d87491",
+    "url": "/coming-soon-react/static/media/logo.ddf22f0b.svg"
+  }
+]);
