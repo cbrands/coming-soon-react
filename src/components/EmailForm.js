2466b37e31c3eb4f680a87ad42cd5dde17bccd41
Charles Brands <charles.brands@gmail.com>
2019-12-29 12:45:56 +0100

    finish styling

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -5,10 +5,8 @@ import './EmailForm.css';
 const EmailForm = () =>  {
   return (
     <form className="email-form"  action="#">
-      <div className="email-input">
-        <input className="email" type="text" name="email" placeholder="Your email address..." aria-label="Email input"/>
-        <p id="warning" className="validation-error">Please provide a valid email address</p>
-      </div>
+      <input className="email" type="text" name="email" placeholder="Your email address..." aria-label="Email input"/>
+      <p id="warning" className="validation-error">Please provide a valid email address</p>
       <input className="submit" type="submit" value="Notify me" aria-label="Submit button"/>
     </form>
   );
