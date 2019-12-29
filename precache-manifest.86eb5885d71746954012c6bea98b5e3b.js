4cab5787e3b1e50eb6df74acec21bcd504de24f3
Charles Brands <charles.brands@gmail.com>
2019-12-29 19:26:59 +0100

    Updates

---
+++ precache-manifest.86eb5885d71746954012c6bea98b5e3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06f3c2bd89f4f72a1746609f229dabf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d92d5b91deb54cb8b3f4",
+    "url": "/static/css/main.00b99afb.chunk.css"
+  },
+  {
+    "revision": "7f848803102c32f5f557",
+    "url": "/static/js/2.c337a013.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c337a013.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d92d5b91deb54cb8b3f4",
+    "url": "/static/js/main.51c279a2.chunk.js"
+  },
+  {
+    "revision": "5c60f8b4b2ca018d208a",
+    "url": "/static/js/runtime-main.091cc426.js"
+  },
+  {
+    "revision": "92dd5c8fe5e8dde945a6f01abca7e7cc",
+    "url": "/static/media/illustration-dashboard.92dd5c8f.png"
+  },
+  {
+    "revision": "ddf22f0b5d6907a8b879a12905d87491",
+    "url": "/static/media/logo.ddf22f0b.svg"
+  }
+]);
